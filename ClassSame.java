d09deae34fa06bd0f7fcdd1322319239b725937d
BugHunterCommunity <softwe.solution01@gmail.com>
2025-05-20 17:21:00 +0530

    gd at 5:20

--- ClassSame.java
+++ ClassSame.java
@@ -1,6 +1,5 @@
 public class ClassSame {
-    //Same name class create by both but created by RP on 4:45 PM.
-    public void hello(){
-        System.out.println("Hello from RP.");
+    public void hello() {
     }
+    //This is added by Gd 4:44
 }
